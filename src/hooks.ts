5206fe6b32bd27b6eabb027910a618b4aa74ec1b
Manassarn Manoonchai <manassarn@gmail.com>
2022-04-21 00:27:27 +0700

    chore: cleanup

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,9 +1,8 @@
 import cookie from 'cookie';
 const DISCORD_API_URL = 'https://discord.com/api';
-const HOST = import.meta.env.VITE_HOST || 'http://localhost:3000';
+const HOST = import.meta.env.VITE_HOST;
 
 export async function getSession(event) {
-	console.log(event.request.headers.get('cookie'));
 	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
 
 	// if only refresh token is found, then access token has expired. perform a refresh on it.
@@ -11,7 +10,6 @@ export async function getSession(event) {
 		const discord_request = await fetch(
 			`${HOST}/auth/refresh?refresh_token=${cookies.refresh_token}`
 		);
-		console.log({ discord_request });
 		const discord_response = await discord_request.json();
 
 		if (discord_response.access_token) {
